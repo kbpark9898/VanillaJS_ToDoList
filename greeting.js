039ec5ea2db4cf446d7ebfe0465d770008b41b48
kbpark9898 <kbpark989@gmail.com>
2021-02-24 23:31:00 +0900

    #3.2 saving user name part two

--- greeting.js
+++ greeting.js
@@ -1,29 +1,43 @@
 const form = document.querySelector(".js-form"),
 input = form.querySelector("input"),
-gretting = document.querySelector("js-grettings");
+gretting = document.querySelector(".js-gretting");
 
 const user = "currntUser",
-showing_cn = "shwoing";
+showing_cn = "showing";
 
 
-function paintGretting(test){
+function paintGretting(text){
     form.classList.remove(showing_cn);
     gretting.classList.add(showing_cn);
-    gretting.innerText(`hello ${text}`);
+    gretting.innerText=`hello ${text}`;
 }
 
 
 function loadName(){
     const userName = localStorage.getItem(user);
     if(userName ==null){
-        //
+        askForName();
     }
     else{
         paintGretting(userName);
     }
 }
+function saveName(text){
+    localStorage.setItem(user, text);
 
+}
+function handleSubminEvent(event){
+    event.preventDefault();
+    const userName = input.value;
+    paintGretting(userName);
+    saveName(userName);
 
+}
+function askForName(){
+    form.classList.add(showing_cn)
+    form.addEventListener("submit", handleSubminEvent)
+
+}
 function init(){
     loadName();
 }
