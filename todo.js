e98b8e46e552dcb40db90e24898237d14120dbd8
kbpark9898 <kbpark989@gmail.com>
2021-02-25 15:22:49 +0900

    #3.5 Making a To Do List part Two

--- todo.js
+++ todo.js
@@ -2,7 +2,7 @@ const todoForm = document.querySelector(".js-todoForm"),
 todoInput = todoForm.querySelector("input"),
 todoList = document.querySelector(".js-todoList");
 
-
+const toDos =[]
 const todo_key = "todoList";
 function paintTodo(text){
     const item = document.createElement('li');
@@ -10,9 +10,16 @@ function paintTodo(text){
     const span = document.createElement('span');
     span.innerText=text;
     deleteBtn.innerText='DEL';
+    const todoID = toDos.length+1;
+    const todoObj={
+        text: text,
+        id = todoID
+    }
     item.appendChild(span);
     item.appendChild(deleteBtn);
     todoList.appendChild(item);
+    toDos.push(todoObj);
+    saveTodoList(todoObj);
 }
 
 function handleSubmit(event){
@@ -21,12 +28,16 @@ function handleSubmit(event){
     paintTodo(currentValue);
     todoInput.value='';
 }
+function saveTodoList(todos){
+    localStorage.setItem(todo_key, JSON.stringify(todos));
+}
 function loadTodoList(){
     const load = localStorage.getItem(todo_key);
-    if(load==null){
-
-    }else{
-
+    if(load !== null){
+        const parsedTodoList = JSON.parse(load);
+        parsedTodoList.forEach(function(todo){
+            paintTodo(todo.text);
+        })
     }
 
 }
